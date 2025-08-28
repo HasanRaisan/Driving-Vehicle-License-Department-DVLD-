6473972142187a0d94dcdf403edfc877bbc2502d
Hasan Raisan <contact@example.com>
2025-08-28 11:08:55 +0300

    Add connection string to WinForms App.config for Data Access Layer

--- clsDataAccessLayer/clsDataAccessSettings.cs
+++ clsDataAccessLayer/clsDataAccessSettings.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Configuration;
-
+﻿using System.Configuration;
 
 namespace ConnectionDataBaseLincense
 {
     public class clsDataAccessSettings
     {
-        public static string ConnectionString = "Server=.;Database=DVLD;User Id=sa;Password=sa123456;";
-       // public static string ConnectionString = ConfigurationManager.AppSettings["DVLDconnection"];
+        public static string ConnectionString =
+            ConfigurationManager.ConnectionStrings["DVLDconnection"].ConnectionString;
     }
 }
